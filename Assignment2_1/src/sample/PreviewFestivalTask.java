e52ee86db636afa5a1ae645b7a800358185d2d23
pranavan10 <pranavan10@github.com>
2019-09-25 18:28:45 +1200

    Need to Restart for espeak

---
+++ Assignment2_1/src/sample/PreviewFestivalTask.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.concurrent.Task;
+
+public class PreviewFestivalTask extends Task {
+/*echo "hello" | festival --tts*/
+	
+	@Override
+    protected Object call() throws Exception {
+    	
+        try {
+        	
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+	}
+}
--- Assignment2_1/src/sample/SelectionController.java
+++ Assignment2_1/src/sample/SelectionController.java
@@ -1,14 +1,18 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class SelectionController implements Initializable {
 	@FXML
@@ -20,16 +24,38 @@ public class SelectionController implements Initializable {
 	@FXML
 	private ComboBox voice;
 	private String _wLines;
+	private Alert alert = new Alert(AlertType.INFORMATION);
+	private String[] words;
 	
 	public void setLines(String largeLines){
 		_wLines=largeLines;
 		lines.setText(largeLines);
+		
+	}
+	
+	public void preview(ActionEvent event) throws IOException {
+		words= selected_lines.getText().split("\\s+");
+		if(words.length>30) {
+			System.out.println("yo");
+			
+		}else if(voice.getValue().equals("festival")) {
+			System.out.println("ho");
+		}else if (voice.getValue().equals("espeak")) {
+			PreviewESpeakTask espeaktask = new PreviewESpeakTask(selected_lines.getText());
+    		Thread thread = new Thread(espeaktask);
+    		thread.start();
+		}
+		
 	}
 	
 	@Override
     public void initialize(URL location, ResourceBundle resources) {
 		lines.setWrapText(true);
 		selected_lines.setWrapText(true);
+		
+		voice.getItems().addAll("festival","espeak");
+		voice.getSelectionModel().selectFirst();
+		
     }
 
 }
