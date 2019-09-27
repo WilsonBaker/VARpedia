9b0f75b530cf28502544a09f92ccb31b5e32ed96
pranavan10 <pranavan10@github.com>
2019-09-27 21:15:20 +1200

    Start of Flickr

--- Assignment2_1/src/sample/CreateFlickrTask.java
+++ Assignment2_1/src/sample/CreateFlickrTask.java
@@ -1,5 +1,21 @@
 package sample;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import javax.imageio.ImageIO;
+
+import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.FlickrException;
+import com.flickr4java.flickr.REST;
+import com.flickr4java.flickr.photos.Photo;
+import com.flickr4java.flickr.photos.PhotoList;
+import com.flickr4java.flickr.photos.PhotosInterface;
+import com.flickr4java.flickr.photos.SearchParameters;
+import com.flickr4java.flickr.photos.Size;
+
 import javafx.concurrent.Task;
 
 public class CreateFlickrTask extends Task{
@@ -18,11 +34,65 @@ public class CreateFlickrTask extends Task{
 		}
 		
 	}
+	public static String getAPIKey(String key) throws Exception {
+		// TODO fix the following based on where you will have your config file stored
+
+		String config = System.getProperty("user.dir") 
+				+ System.getProperty("file.separator")+ "flickr-api-keys.txt"; 
+		
+//		String config = System.getProperty("user.home")
+//				+ System.getProperty("file.separator")+ "bin" 
+//				+ System.getProperty("file.separator")+ "flickr-api-keys.txt"; 
+		File file = new File(config); 
+		BufferedReader br = new BufferedReader(new FileReader(file)); 
+		
+		String line;
+		while ( (line = br.readLine()) != null ) {
+			if (line.trim().startsWith(key)) {
+				br.close();
+				return line.substring(line.indexOf("=")+1).trim();
+			}
+		}
+		br.close();
+		throw new RuntimeException("Couldn't find " + key +" in config file "+file.getName());
+	}
 	@Override
     protected Object call() throws Exception {
-		System.out.println(_number);
-		System.out.println(_wikitSearch);
-		System.out.println(_name);
+		try {
+			String apiKey = getAPIKey("apiKey");
+			String sharedSecret = getAPIKey("sharedSecret");
+
+			Flickr flickr = new Flickr(apiKey, sharedSecret, new REST());
+			
+			String query = "bicycle";
+			int resultsPerPage = 5;
+			int page = 0;
+			
+	        PhotosInterface photos = flickr.getPhotosInterface();
+	        SearchParameters params = new SearchParameters();
+	        params.setSort(SearchParameters.RELEVANCE);
+	        params.setMedia("photos"); 
+	        params.setText(query);
+	        
+	        PhotoList<Photo> results = photos.search(params, resultsPerPage, page);
+	        System.out.println("Retrieving " + results.size()+ " results");
+	        
+	        for (Photo photo: results) {
+	        	try {
+	        		BufferedImage image = photos.getImage(photo,Size.LARGE);
+		        	String filename = query.trim().replace(' ', '-')+"-"+System.currentTimeMillis()+"-"+photo.getId()+".jpg";
+		        	File outputfile = new File("downloads",filename);
+		        	ImageIO.write(image, "jpg", outputfile);
+		        	System.out.println("Downloaded "+filename);
+	        	} catch (FlickrException fe) {
+	        		System.err.println("Ignoring image " +photo.getId() +": "+ fe.getMessage());
+				}
+	        }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 		return null;
 	}
 }
